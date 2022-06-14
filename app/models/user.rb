af4149e110a59c2e5699fd681db8448229eabbe3
che30 <che.blancoo58@gmail.com>
2022-06-14 22:42:55 +0100

    Add user request tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   # belongs_to :doctor, class_name: "User", foreign_key: "doctor_id", optional: true
   # has_many :doctors, class_name: "User", foreign_key: "doctor_id"
-  validates_presence_of :first_name, :last_name, :email, :password, :password_digest
+  validates_presence_of :first_name, :last_name, :email, :password_digest
   validates :email, uniqueness: true
   has_many :appointments
 end
