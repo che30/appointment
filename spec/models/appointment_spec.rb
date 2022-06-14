792be0e8bdde61604b2f131282d7d70e9a0b7411
che30 <che.blancoo58@gmail.com>
2022-06-14 22:50:41 +0100

    Add model specs for Appointment and user

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:message) }
+  it { should validate_presence_of(:time_slot) }
+  it { should validate_presence_of(:date_of_appointment) }
+  it { should belong_to(:user)}
+  it { should belong_to(:doctor)}
 end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -4,11 +4,11 @@ require 'rails_helper'
 #   pending "add some examples to (or delete) #{__FILE__}"
 # end
 
-RSpec.describe User, type: :model do
+RSpec.describe Doctor, type: :model do
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
   # it { should validate_presence_of(:specialty) }
+  it { should validate_presence_of(:specialty) }
   it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password_digest) }
   # it { should have_many(:measurements).dependent(:destroy) }
 end
