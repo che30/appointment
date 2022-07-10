a703685162501431336237ceb80ae6ba8f400a70
che30 <che.blancoo58@gmail.com>
2022-07-10 16:28:38 +0100

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 allow do
   origins 'https://cheappointment.herokuapp.com'
 
-  resource 'auth/login',
+  resource '/auth/login',
     :headers => :any,
     :methods => [:post]
   resource '/signup',
