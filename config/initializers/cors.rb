c66257b6e71bb7bd9c549cedad33f779ec4db7a1
che30 <che.blancoo58@gmail.com>
2022-07-10 20:35:30 +0100

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -38,15 +38,15 @@ allow do
     
 end
 end
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins "*"
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins "*"
 
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-end
+#     resource "*",
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
+# end
 # Rails.application.config.middleware.insert_before 0, Rack::Cors do
 # allow do
 #   origins 'http://localhost:3000'
